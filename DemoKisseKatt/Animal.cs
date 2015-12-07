eec46b7ee57908dc5ce396f416e1ea6d26d04ca9
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-07 13:25:17 +0100

    KitCatt

---
+++ DemoKisseKatt/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKisseKatt
+{
+    abstract class Animal
+    {
+        public string Name { get; set; }
+        public abstract void PrintInfo();
+    }
+}
---
+++ DemoKisseKatt/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKisseKatt
+{
+    class Cat : Animal
+    {
+        private string djur = "katt";
+
+        public override void PrintInfo() //Sometimes prononced as productInfo.. ihihi
+        {
+            Console.WriteLine($"Djur: {djur}, Name: {Name}");
+        }
+    }
+    class Dog : Animal
+    {
+        private string djur = "hund";
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Djur: {djur}, Name: {Name}");
+        }
+    }
+}
