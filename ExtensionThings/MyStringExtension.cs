f5a25927e11a580cf27faed49737add3e78b10af
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-07 14:27:01 +0100

    ExtensionMethod

---
+++ ExtensionThings/MyStringExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionThings
+{
+    public static class MyStringExtension
+    {
+        public static string Dotify(this string text)
+        {
+            string result = text.Trim();
+            if (result[result.Length - 1] != '.')
+                return result + '.';
+            else
+                return result;
+        }
+        public static string PlussPussa(this string tal)
+        {
+            return tal + tal;
+        }
+    }
+}
---
+++ ExtensionThings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = MyStringExtension.Dotify("Hej");
+            Console.WriteLine(name);
+            Console.WriteLine("Kalle".Dotify());
+            Console.WriteLine("Freddie ".PlussPussa());
+            Console.ReadLine();
+        }
+    }
+}
