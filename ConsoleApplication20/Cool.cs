018da1cda6c71b8e350e4dc35a8bbe513be1038f
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-07 11:51:58 +0100

    l

--- ConsoleApplication20/Cool.cs
+++ ConsoleApplication20/Cool.cs
@@ -8,5 +8,19 @@ namespace ConsoleApplication20
 {
     class Cool
     {
+        public int Number { get; set; }
+
+        //override innebär skriver en ny och förbättrad version som vi ärvt från en basklass
+        //override tar befälet över metoden
+        //basklassens virtual säger att det är ok att göra en override
+        //kan göras på metoder, properties, events, indexer
+
+        //Här görs override från Object F12 och titta:
+        Object obj;
+        public override string ToString()
+        {
+            return Number.ToString();
+        }
+        
     }
 }
