20f0da91ae84cf250875fc45d73db97092d84fd0
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-09 11:46:43 +0100

    Poco

---
+++ Inventarie/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventarie
+{
+    class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public int  Quantity { get; set; }
+
+        public Product(string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"ProductName:{Name}, Quantity{Quantity}";
+        }
+
+        public int CompareTo(Product other)
+        {
+            return Quantity.CompareTo(other.Quantity);
+        }
+    }
+}
