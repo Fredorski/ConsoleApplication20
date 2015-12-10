bf87a23c62e447b871a6ad4a144007d55b964ba1
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-10 09:44:30 +0100

    Ctor

--- Structisar/Point.cs
+++ Structisar/Point.cs
@@ -10,5 +10,23 @@ namespace Structisar
     {
         public int X;
         public int Y;
+
+        /*
+            - man får inte göra en tom konstruktor
+            - om man gör en överlagrad konstruktor måste man sätta
+              ett värde på alla fält!
+
+            */
+        public Point(int x)
+        {
+            X = x;
+            Y = 7;
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 }
--- Structisar/Program.cs
+++ Structisar/Program.cs
@@ -11,7 +11,11 @@ namespace Structisar
         static void Main(string[] args)
         {
             //StructDemo1();
-
+            Point ett;
+            ett.X = 4;
+            ett.Y = 5;
+            ett = new Point(0);
+            Console.WriteLine("X:{0}, Y:{1}", ett.X, ett.Y);
             Console.ReadLine();
         }
 
@@ -21,7 +25,7 @@ namespace Structisar
             koordinat.X = 66;
             koordinat.Y = 88;
 
-            Koordinat origo = new Koordinat() {X = 50, Y = 50};
+            Koordinat origo = new Koordinat() { X = 50, Y = 50 };
             origo.Z = "19";
             origo = new Koordinat();
             /*Nollställer strukten
