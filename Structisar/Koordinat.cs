fd51eed0aabf52bb8ec6508cfb5337c9dbfdc90a
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-10 09:35:25 +0100

    Structisar

---
+++ Structisar/Koordinat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structisar
+{
+    /*
+    * en struct är en egendefinierad värdetyp
+    * har inga referenser till heapen
+    * är implicit sealed
+    * är public per default
+    * skrivs som vi skriver en klass men har nyckelordet struct
+    * alla de primitiva typerna är structar:
+          ex, bool, int, long, double, float, char, DateTime
+    * ungefär 3ggr snabbare än en class om 5 fält finns
+    * kan inte vara null, pga kan inte referera till heapen
+
+        */
+
+
+    public struct Koordinat 
+    {
+        public int X;
+        public int Y;
+        public string Z;
+
+     
+        //public int Z = 9; //Does not compute, vi får inte sätta värde på fält i en struct
+        public override string ToString()
+        {
+            return $"X:{X}, Y:{Y}, Z:{Z}";
+        }
+    }
+
+    //class StructXYZ: koordinat
+    //{
+    //    //Kan och får inte ärva från en struct 
+    //}
+}
---
+++ Structisar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structisar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StructDemo1();
+
+            Console.ReadLine();
+        }
+
+        private static void StructDemo1()
+        {
+            Koordinat koordinat;
+            koordinat.X = 66;
+            koordinat.Y = 88;
+
+            Koordinat origo = new Koordinat() {X = 50, Y = 50};
+            origo.Z = "19";
+            origo = new Koordinat();
+            /*Nollställer strukten
+            alla fält får defaultvärdet för respektive datatyp
+            observera att string fick null i sig nu*/
+            Console.WriteLine(origo);
+        }
+    }
+}
