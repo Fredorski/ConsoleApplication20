4cb7a797f2fa7eb684d14d27811dda63be11ff5b
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-10 15:06:39 +0100

    Enumis

---
+++ EnumMeny/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EnumMeny
+{
+    enum Menu {AddItem, LoseItem, CreateSome, Awsome, Avsluta }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal;
+            Console.WriteLine("Skriv 0-4:");
+            if (int.TryParse(Console.ReadLine(), out tal))
+            {
+                Menu menu = (Menu) tal;
+                switch (menu)
+                {
+                    case Menu.AddItem:
+                        Console.WriteLine(Menu.AddItem);
+                        break;
+                    case Menu.LoseItem:
+                        Console.WriteLine(Menu.LoseItem);
+                        break;
+                    case Menu.CreateSome:
+                        Console.WriteLine(Menu.CreateSome);
+                        break;
+                    case Menu.Awsome:
+                        Console.WriteLine(Menu.Awsome);
+                        break;
+                    case Menu.Avsluta:
+                        Console.WriteLine(Menu.Avsluta);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
