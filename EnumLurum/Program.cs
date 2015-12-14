c49eda4d92835f5c27bdf200ee062d6ea17a6bdf
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-14 09:19:26 +0100

    Menu lurum

---
+++ EnumLurum/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumLurum
+{
+    enum Menu
+    {
+        Add=1, Substract, Multiply, Devide, End
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMenuWithEnums();
+            bool loopa = true;
+            do
+            {
+                int tal;
+                if (int.TryParse(Console.ReadLine(), out tal))
+                {
+                    Menu menu = (Menu) tal;
+                    switch (menu)
+                    {
+                        case Menu.Add:
+                            Console.WriteLine(Menu.Add);
+                            break;
+                        case Menu.Substract:
+                            Console.WriteLine(Menu.Substract);
+                            break;
+                        case Menu.Multiply:
+                            Console.WriteLine(Menu.Multiply);
+                            break;
+                        case Menu.Devide:
+                            Console.WriteLine(Menu.Devide);
+                            break;
+                        case Menu.End:
+                            Console.WriteLine(Menu.End);
+                            loopa = false;
+                            break;
+                        default:
+                            //throw new ArgumentOutOfRangeException();
+                            Console.WriteLine("Something wnt wrong... Please try again");
+                            break;
+                    }
+                }
+            } while (loopa); 
+            Console.ReadLine();
+        }
+
+        private static void PrintMenuWithEnums()
+        {
+            int counter = 1;
+            foreach (var item in Enum.GetNames(typeof (Menu)))
+            {
+                Console.WriteLine("{0}: {1}", counter++, item);
+            }
+            Console.WriteLine("Please enter a number from the list above:");
+        }
+    }
+}
--- EnumMeny/Program.cs
+++ EnumMeny/Program.cs
@@ -2,39 +2,60 @@
 
 namespace EnumMeny
 {
-    enum Menu {AddItem, LoseItem, CreateSome, Awsome, Hejsan, Avsluta }
+    enum Menu { AddItem, LoseItem, CreateSome, Awsome, Hejsan, Avsluta }
     class Program
     {
         static void Main(string[] args)
         {
-            int tal;
-            Console.WriteLine("Skriv 0-4:");
-            if (int.TryParse(Console.ReadLine(), out tal))
+            bool loopa = true;
+            do
             {
-                Menu menu = (Menu) tal;
-                switch (menu)
+                PrintMenuFromEnums();
+                int tal;
+                Console.WriteLine("Skriv 0-4:");
+
+
+
+                if (int.TryParse(Console.ReadLine(), out tal))
                 {
-                    case Menu.AddItem:
-                        Console.WriteLine(Menu.AddItem);
-                        break;
-                    case Menu.LoseItem:
-                        Console.WriteLine(Menu.LoseItem);
-                        break;
-                    case Menu.CreateSome:
-                        Console.WriteLine(Menu.CreateSome);
-                        break;
-                    case Menu.Awsome:
-                        Console.WriteLine(Menu.Awsome);
-                        break;
-                    case Menu.Avsluta:
-                        Console.WriteLine(Menu.Avsluta);
-                        break;
+                    Menu menu = (Menu)tal;
+                    switch (menu)
+                    {
+                        case Menu.AddItem:
+                            Console.WriteLine(Menu.AddItem);
+                            break;
+                        case Menu.LoseItem:
+                            Console.WriteLine(Menu.LoseItem);
+                            break;
+                        case Menu.CreateSome:
+                            Console.WriteLine(Menu.CreateSome);
+                            break;
+                        case Menu.Awsome:
+                            Console.WriteLine(Menu.Awsome);
+                            break;
+                        case Menu.Avsluta:
+                            Console.WriteLine(Menu.Avsluta);
+                            loopa = false;
+                            break;
                         case Menu.Hejsan: //Nytt enum inlaggt tjihoo, 
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
+                            break;
+                        default:
+                            Console.WriteLine("Something went wrong... Try again to write a number corresponding to the menu choises!");
+                            //throw new ArgumentOutOfRangeException();
+                            break;
+                    }
                 }
-                Console.ReadLine();
+            } while (loopa);
+            Console.WriteLine("Program has stoped! Press ENTER!");
+            Console.ReadLine();
+        }
+
+        private static void PrintMenuFromEnums()
+        {
+            int counter = 0;
+            foreach (string item in Enum.GetNames(typeof(Menu)))
+            {
+                Console.WriteLine("{0}: {1}", counter++, item);
             }
         }
     }
